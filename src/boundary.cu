0200ed9178429d85f4a4b5febe99f73a44f534d6
Mathiasb17 <mathiasb17@gmail.com>
2017-05-26 17:01:24 +0200

    removed garbage

--- src/boundary.cu
+++ src/boundary.cu
@@ -25,20 +25,20 @@ extern "C"
 
 		//cudaMemcpy
 		cudaMemcpy(d_vbi, vbi, num_boundaries*sizeof(float), cudaMemcpyHostToDevice);
-		/*cudaMemcpy(boundary_pos, d_boundary_pos, num_boundaries*sizeof(float)*4, cudaMemcpyHostToDevice);*/
+		cudaMemcpy(d_boundary_pos, boundary_pos, num_boundaries*sizeof(float)*4, cudaMemcpyHostToDevice);
 
 		/*//kernel call*/
 		
-		/*unsigned int numThreads, numBlocks;*/
-		/*computeGridSiz(num_boundaries, 256, numBlocks, numThreads);*/
+		unsigned int numThreads, numBlocks;
+		computeGridSiz(num_boundaries, 256, numBlocks, numThreads);
 
-		/*[>computeVbi<<<numBlocks, numThreads>>>((float4*)boundary_pos, d_vbi, ir,num_boundaries);<]*/
+		computeVbi<<<numBlocks, numThreads>>>((float4*)boundary_pos, d_vbi, ir,num_boundaries);
 
 		/*//transfer back to host mem*/
-		/*cudaMemcpy(vbi, d_vbi, num_boundaries*sizeof(float), cudaMemcpyDeviceToHost);*/
+		cudaMemcpy(vbi, d_vbi, num_boundaries*sizeof(float), cudaMemcpyDeviceToHost);
 
 		/*//cudaFree*/
-		/*cudaFree(d_boundary_pos);*/
-		/*cudaFree(d_vbi);*/
+		cudaFree(d_boundary_pos);
+		cudaFree(d_vbi);
 	}
 }
