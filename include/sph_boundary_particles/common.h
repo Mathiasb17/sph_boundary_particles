064a1544b68b28e50401552dfac759ece1850da7
Mathiasb17 <mathiasb17@gmail.com>
2017-06-26 16:01:05 +0200

    finished refactor

--- include/sph_boundary_particles/common.h
+++ include/sph_boundary_particles/common.h
@@ -6,7 +6,15 @@
 #include <helper_cuda.h>
 
 
-#if 1
+#ifdef DOUBLE_PRECISION
+typedef double SReal;
+typedef double2 SVec2;
+typedef double3 SVec3;
+typedef double4 SVec4;
+#define make_SVec3 make_double3
+#define make_SVec4 make_double4
+
+#else
 typedef float SReal;
 typedef int2 SVec2i;
 typedef float2 SVec2;
@@ -14,13 +22,7 @@ typedef float3 SVec3;
 typedef float4 SVec4;
 #define make_SVec3 make_float3
 #define make_SVec4 make_float4
-#else
-typedef double SReal;
-typedef double2 SVec2;
-typedef double3 SVec3;
-typedef double4 SVec4;
-#define make_SVec3 make_double3
-#define make_SVec4 make_double4
+
 #endif
 
 #endif /* ifndef COMMON_H */
